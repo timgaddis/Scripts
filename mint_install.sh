fdf09daf3194807c86f287978744c4f619eaa47b
Tim Gaddis <timgaddis@gmail.com>
2016-12-18 10:47:44 -0700

    Updated nvidia driver. Removed python request and oauth libraries.

--- mint_install.sh
+++ mint_install.sh
@@ -51,7 +51,7 @@ sudo apt update
 sudo apt upgrade -y
 
 print_title "System apps"
-sudo apt install -y nvidia-370 nvidia-settings nvidia-prime ntfs-config mint-meta-codecs encfs ttf-mscorefonts-installer fonts-droid-fallback nodejs
+sudo apt install -y nvidia-375 nvidia-settings nvidia-prime ntfs-config mint-meta-codecs encfs ttf-mscorefonts-installer fonts-droid-fallback nodejs
 
 print_title "Other apps"
 sudo apt install -y wine1.8 pan gpodder steam qbittorrent hwinfo font-manager oracle-java8-installer oracle-java8-set-default virtualbox-5.1
@@ -60,8 +60,9 @@ sudo apt install -y jq keepassx p7zip-full hexchat lynx inkscape xsltproc menuli
 # backintime-qt4 gnome-encfs-manager nemo-dropbox stellarium
 
 print_title "Development apps"
-sudo apt install -y python-requests python-requests-oauthlib python-oauthlib build-essential python-software-properties g++ git gitg
-	
+sudo apt install -y build-essential python-software-properties g++ git gitg
+# python-requests python-requests-oauthlib python-oauthlib
+
 #print_title "Latex apps"
 #sudo apt install -y texlive-full texstudio
 
