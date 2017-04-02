002f8a9bfae48fe56a60445c04a1d3f4802487ce
Tim Gaddis <timgaddis@gmail.com>
2017-04-02 12:12:59 -0600

    Removed Nvidia PPA and drivers. Updated Libreoffice PPA.

--- mint_install.sh
+++ mint_install.sh
@@ -21,7 +21,7 @@ wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu xenial-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
 
 print_title "PPA install"
-sudo apt add-repository -y ppa:graphics-drivers/ppa                     # Nvidia graphic drivers
+#sudo apt add-repository -y ppa:graphics-drivers/ppa                     # Nvidia graphic drivers
 #sudo add-apt-repository -y ppa:bit-team/stable                          # Back in Time
 sudo apt add-repository -y ppa:thp/gpodder                              # gPodder
 #sudo apt add-repository -y ppa:numix/ppa                                # Numix GTK themes
@@ -33,7 +33,7 @@ sudo apt add-repository -y ppa:menulibre-dev/daily                      # Menu L
 sudo apt add-repository -y ppa:gwendal-lebihan-dev/hexchat-stable       # HexChat IRC client
 sudo apt add-repository -y ppa:webupd8team/java                         # Java 6/7/8/9
 #sudo apt add-repository -y ppa:gencfsm/ppa                              # GNOME Encfs Manager
-sudo apt add-repository -y ppa:libreoffice/ppa			                # LibreOffice 5.3.X
+sudo apt add-repository -y ppa:libreoffice/libreoffice-5-3              # LibreOffice 5.3.X
 sudo apt add-repository -y ppa:webupd8team/sublime-text-3               # Sublime Text 3
 sudo apt add-repository -y ppa:qbittorrent-team/qbittorrent-stable      # qBittorrent
 sudo apt add-repository -y ppa:inkscape.dev/stable                      # Inkscape
@@ -52,12 +52,11 @@ sudo apt update
 sudo apt upgrade -y
 
 print_title "System apps"
-sudo apt install -y nvidia-378 nvidia-settings nvidia-prime ntfs-config mint-meta-codecs encfs ttf-mscorefonts-installer fonts-droid-fallback nodejs
+sudo apt install -y ntfs-config mint-meta-codecs encfs ttf-mscorefonts-installer fonts-droid-fallback nodejs
+# nvidia-378 nvidia-settings nvidia-prime libcuda1-378 nvidia-opencl-icd-378
 
 print_title "Other apps"
-sudo apt install -y wine1.8 pan gpodder steam qbittorrent hwinfo font-manager oracle-java8-installer oracle-java8-set-default virtualbox-5.1
-sudo apt install -y adobe-flashplugin grub-customizer polly plank pypar2 gparted curl deja-dup chromium-browser conky-all sublime-text-installer
-sudo apt install -y jq keepassx p7zip-full hexchat lynx inkscape xsltproc menulibre gimp-plugin-registry gimp-gmic gimp
+sudo apt install -y wine1.8 pan gpodder steam qbittorrent hwinfo font-manager oracle-java8-installer oracle-java8-set-default virtualbox-5.1 adobe-flashplugin grub-customizer polly plank pypar2 gparted curl deja-dup chromium-browser conky-all sublime-text-installer jq keepassx p7zip-full hexchat lynx inkscape xsltproc menulibre gimp-plugin-registry gimp-gmic gimp
 # backintime-qt4 gnome-encfs-manager nemo-dropbox stellarium
 
 print_title "Development apps"
@@ -69,8 +68,7 @@ sudo apt install -y build-essential python-software-properties g++ git gitg
 
 print_title "Themes"
 #sudo apt install -y numix-gtk-theme numix-icon-theme numix-icon-theme-circle
-sudo apt install -y moka-icon-theme faba-icon-theme moka-gnome-shell-theme moka-gtk-theme
-sudo apt install -y gtk2-engines-murrine vertex-theme arc-theme
+sudo apt install -y moka-icon-theme faba-icon-theme moka-gnome-shell-theme moka-gtk-theme gtk2-engines-murrine vertex-theme arc-theme
 
 # Add user to vboxusers group
 sudo usermod -a -G vboxusers tgaddis
