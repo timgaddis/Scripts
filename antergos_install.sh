b079426e4d065008de2d9c587d08c3a1705c82f4
Tim Gaddis <timgaddis@gmail.com>
2017-10-10 15:30:16 -0600

    Added SublimeText repo and removed menulibre.

--- antergos_install.sh
+++ antergos_install.sh
@@ -50,11 +50,17 @@ pause_function
 # pause_function
 
 print_title "Install AUR apps"
-pacaur -S rar gpodder3 spideroak-one polly pypar2 sublime-text-dev grub-customizer menulibre plex-media-server gimp-paint-studio gimp-plugin-pandora
+pacaur -S rar gpodder3 spideroak-one polly pypar2 grub-customizer plex-media-server gimp-paint-studio gimp-plugin-pandora
 pause_function
 
 print_title "Install programming apps"
-pacaur -S google-cloud-sdk gitg jdk nodejs sqlitebrowser
+pacaur -S google-cloud-sdk gitg jdk nodejs sqlitebrowser npm
+pause_function
+
+print_title "Install Sublime Text"
+sudo curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+sudo bash -c 'echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf'
+sudo pacman -Syu sublime-text
 pause_function
 
 print_title "Enable and start plex"
