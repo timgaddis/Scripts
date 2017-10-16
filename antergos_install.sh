a43b884516c47e1b0cf9246ec3ee01dbe51be928
Tim Gaddis <timgaddis@gmail.com>
2017-10-16 12:42:14 -0600

    Removed SublimeText repo.

--- antergos_install.sh
+++ antergos_install.sh
@@ -15,13 +15,6 @@ pause_function() {
     read -e -sn 1 -p "Press a key to continue..."
 }
 
-#print_title "Update mirror list"
-#curl -O https://raw.githubusercontent.com/Gen2ly/armrr/master/armrr
-#chmod +x armrr 
-#./armrr US
-#sudo pacman -Syy
-#pause_function
-
 print_title "Install system apps"
 sudo pacman -S pacaur gparted conky jq gksu lynx python-lxml gtk-engine-murrine galculator ntp gconf-editor x264 ttf-opensans
 pause_function
@@ -35,7 +28,7 @@ pause_function
 # pause_function
 
 print_title "Install apps"
-sudo pacman -S plank deja-dup vlc thunderbird keepass hexchat qbittorrent pan pepper-flash
+sudo pacman -S plank deja-dup vlc thunderbird keepass hexchat qbittorrent pan
 sudo pacman -S gimp gimp-plugin-lqr gimp-plugin-gmic gimp-plugin-fblur gimp-refocus gimp-ufraw
 pause_function
 
@@ -50,18 +43,18 @@ pause_function
 # pause_function
 
 print_title "Install AUR apps"
-pacaur -S rar gpodder3 spideroak-one polly pypar2 grub-customizer plex-media-server gimp-paint-studio gimp-plugin-pandora
+pacaur -S rar gpodder3 spideroak-one polly grub-customizer menulibre plex-media-server gimp-paint-studio gimp-plugin-pandora
 pause_function
 
 print_title "Install programming apps"
-pacaur -S google-cloud-sdk gitg jdk nodejs sqlitebrowser npm
+pacaur -S google-cloud-sdk gitg jdk8 nodejs sqlitebrowser npm sublime-text-dev
 pause_function
 
-print_title "Install Sublime Text"
-sudo curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-sudo bash -c 'echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf'
-sudo pacman -Syu sublime-text
-pause_function
+#print_title "Install Sublime Text"
+#sudo curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+#sudo bash -c 'echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf'
+#sudo pacman -Syu sublime-text
+#pause_function
 
 print_title "Enable and start plex"
 sudo systemctl enable plexmediaserver.service
