6eabcd2bfea8022e464dd9e66009dd5ede6cd16f
Tim Gaddis <timgaddis@gmail.com>
2022-08-31 18:57:30 -0600

    Update endeavour.sh

--- endeavour.sh
+++ endeavour.sh
@@ -86,7 +86,7 @@ pause_function
 print_title "Install Sublime Text"
 sudo curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 sudo bash -c 'echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf'
-sudo pacman -Syu --noconfirm --needed sublime-text
+sudo pacman -Syy --noconfirm sublime-text
 pause_function
 
 print_title "Install themes"
