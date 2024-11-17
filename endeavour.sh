65b13f53f21fddd155562fe407652f938a67d308
Tim Gaddis <timgaddis@gmail.com>
2024-11-17 08:47:40 -0700

    Update endeavour.sh

--- endeavour.sh
+++ endeavour.sh
@@ -51,18 +51,18 @@ sudo pacman -S --noconfirm --needed adobe-source-sans-pro-fonts cantarell-fonts 
 pause_function
 
 print_title "Install apps"
-sudo pacman -S --noconfirm --needed plank deja-dup keepassxc wine vlc qbittorrent steam inkscape borg hexchat gimp mailspring solaar
+sudo pacman -S --noconfirm --needed plank deja-dup keepassxc wine vlc qbittorrent steam inkscape borg hexchat gimp solaar
 pause_function
 
 print_title "Install AUR apps"
-yay -S --noconfirm --needed ferdium kalu gimp-paint-studio megasync-bin vorta google-chrome brother-mfc-j491dw stash-bin fstl pan lightdm-settings
+yay -S --noconfirm --needed ferdium kalu gimp-paint-studio megasync-bin vorta google-chrome stash-bin fstl pan lightdm-settings mailspring
 wget https://mega.nz/linux/repo/Arch_Extra/x86_64/nemo-megasync-x86_64.pkg.tar.zst && sudo pacman -U --noconfirm nemo-megasync-x86_64.pkg.tar.zst
 pause_function
 
 print_title "Install programming apps"
 sudo pacman -S --noconfirm --needed jdk-openjdk openjdk-doc openjdk-src
 sudo pacman -S --noconfirm --needed gitg sqlitebrowser npm libvirt android-tools python-beautifulsoup4 python-feedparser python-numpy python-regex kotlin
-yay -S --noconfirm --needed python-selenium selenium-manager geckodriver chromedriver gitkraken
+yay -S --noconfirm --needed gitkraken python-selenium selenium-manager geckodriver chromedriver
 pause_function
 
 print_title "Install and start plex"
@@ -88,9 +88,15 @@ sudo bash -c 'echo -e "\n[sublime-text]\nServer = https://download.sublimetext.c
 sudo pacman -Syy --noconfirm sublime-text
 pause_function
 
-print_title "Install themes"
+print_title "Install themes and sounds"
 sudo pacman -S --noconfirm --needed arc-icon-theme gtk-engine-murrine elementary-icon-theme gtk-engine-murrine
-yay -S --noconfirm --needed papirus-icon-theme-git plank-theme-arc
+yay -S --noconfirm --needed papirus-icon-theme-git 
+mkdir sound
+cd sound
+wget -O PKGBUILD https://bit.ly/3YffPWX
+wget -O cinnamon-sound-effects.gschema.override https://bit.ly/4eQB3kt
+wget -O cinnamon-sound-effects.install https://bit.ly/4gUCQXn
+makepkg --install
 pause_function
 
 print_title "Mount hard drives"
@@ -107,5 +113,7 @@ print_title "HiDPI fix for GRUB"
 sudo cp /etc/default/grub /etc/default/grub.bak
 sudo sed -i 's/GRUB_GFXMODE=auto/GRUB_GFXMODE=1024x768x32/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo sed -i 's/#IgnorePkg   =/IgnorePkg   = cinnamon-sound-effects nemo-megasync/g' /etc/pacman.conf
 
 echo "Done!!!"
