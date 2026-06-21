f7ab208111de6e4685557ad9bfe9596c50221f9d
Tim Gaddis <timgaddis@gmail.com>
2026-06-21 07:18:58 -0600

    Updated endeavour.sh

--- endeavour.sh
+++ endeavour.sh
@@ -32,8 +32,14 @@ sudo pacman -Syy
 sudo pacman -Rs --noconfirm xed
 
 print_title "Install system apps"
-sudo pacman -S --noconfirm --needed conky jq lynx python-lxml mate-calc cmake eog eog-plugins unace arj expac yajl p7zip gparted gnome-font-viewer gedit numlockx gnome-keyring xdg-desktop-portal-gtk evince yt-dlp
+sudo pacman -S --noconfirm --needed yay conky jq lynx python-lxml mate-calc cmake eog eog-plugins unace arj expac yajl p7zip gparted gnome-font-viewer gedit numlockx gnome-keyring xdg-desktop-portal-gtk evince yt-dlp ffmpegthumbnailer
 pause_function
+
+#print_title "Setup Bluetooth apps"
+#sudo pacman -S --noconfirm --needed bluez bluez-utils blueman
+#sudo systemctl enable bluetooth.service
+#sudo systemctl start bluetooth.service 
+#pause_function
 
 print_title "Install Raedon drivers"
 sudo pacman -S --noconfirm --needed xf86-video-ati vulkan-radeon lib32-vulkan-radeon vulkan-tools
@@ -51,14 +57,13 @@ sudo pacman -S --noconfirm --needed adobe-source-sans-pro-fonts cantarell-fonts 
 pause_function
 
 print_title "Install apps"
-sudo pacman -S --noconfirm --needed plank deja-dup keepassxc wine vlc vlc-plugins-all qbittorrent steam inkscape borg hexchat gimp telegram-desktop vorta pan
+sudo pacman -S --noconfirm --needed plank keepassxc wine vlc vlc-plugins-all qbittorrent steam inkscape borg gimp telegram-desktop vorta pan proton-vpn-gtk-app
 pause_function
 
 print_title "Install AUR apps"
-yay -S --noconfirm --needed ferdium-bin kalu megasync google-chrome stash-bin fstl lightdm-settings electron33-bin mailspring-bin
+yay -S --noconfirm --needed ferdium-bin megasync google-chrome stash-bin fstl lightdm-settings mailspring-bin octopi qdirstat chatterino2-bin
 wget https://mega.nz/linux/repo/Arch_Extra/x86_64/nemo-megasync-x86_64.pkg.tar.zst && sudo pacman -U --noconfirm nemo-megasync-x86_64.pkg.tar.zst
 pause_function
-# gimp-paint-studio
 
 print_title "Install Flatpak"
 sudo pacman -S --noconfirm --needed flatpak
@@ -67,20 +72,29 @@ pause_function
 
 print_title "Install programming apps"
 sudo pacman -S --noconfirm --needed jdk-openjdk openjdk-doc openjdk-src
-sudo pacman -S --noconfirm --needed gitg sqlitebrowser npm libvirt android-tools python-beautifulsoup4 python-feedparser python-numpy python-regex kotlin
+sudo pacman -S --noconfirm --needed gitg sqlitebrowser npm libvirt android-tools python-beautifulsoup4 python-feedparser python-numpy python-regex kotlin python-pycryptodome python-pip
 yay -S --noconfirm --needed gitkraken python-selenium selenium-manager geckodriver chromedriver
 pause_function
 
-print_title "Install and start plex"
-yay -S --noconfirm --needed plex-media-server
-sudo systemctl enable plexmediaserver.service
-sudo systemctl start plexmediaserver.service
+#print_title "Install and start plex"
+#yay -S --noconfirm --needed plex-media-server
+#sudo systemctl enable plexmediaserver.service
+#sudo systemctl start plexmediaserver.service
+#pause_function
+
+print_title "Install and start Jellyfin"
+yay -S --noconfirm --needed jellyfin-server jellyfin-web
+sudo systemctl enable jellyfin.service
+sudo systemctl start jellyfin.service
 pause_function
 
 print_title "Install and start Razer drivers"
-yay -S razergenie
+sudo pacman -S --noconfirm openrazer-daemon
+yay -S --noconfirm --needed razergenie-git
+# openrazer-meta-git
 # The daemon requires you add the user to the `plugdev` group:
-sudo gpasswd -a tgaddis plugdev
+#sudo gpasswd -a tgaddis plugdev
+sudo gpasswd -a tgaddis openrazer
 # Enable the OpenRazer daemon systemd service:
 systemctl --user enable openrazer-daemon.service
 pause_function
@@ -103,8 +117,9 @@ sudo pacman -Syy --noconfirm sublime-text
 pause_function
 
 print_title "Install themes and sounds"
-sudo pacman -S --noconfirm --needed gtk-engine-murrine elementary-icon-theme gtk-engine-murrine
-yay -S --noconfirm --needed papirus-icon-theme-git flat-remix flat-remix-gtk xcursor-simp1e-adw
+# yay -S --noconfirm --needed arc-gtk-theme-git
+#sudo pacman -S --noconfirm --needed gtk-engine-murrine elementary-icon-theme
+# flat-remix flat-remix-gtk xcursor-simp1e-adw
 mkdir sound
 cd sound
 wget -O PKGBUILD https://bit.ly/3YffPWX
@@ -112,7 +127,6 @@ wget -O cinnamon-sound-effects.gschema.override https://bit.ly/4eQB3kt
 wget -O cinnamon-sound-effects.install https://bit.ly/4gUCQXn
 makepkg --install
 pause_function
-# arc-icon-theme
 
 print_title "Mount hard drives"
 cd /
